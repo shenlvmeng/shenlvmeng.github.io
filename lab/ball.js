28ff331b5a2224c02b06bcf1dc7472d835a3b993
Zinc <xin_yuanming@163.com>
2017-01-28 22:14:29 +0800

    add bounce behavior to Lab001

--- lab/ball.js
+++ lab/ball.js
@@ -10,34 +10,47 @@
 		status = 'on',
 
 		nextTick = function () {
+			d3.select('svg').selectAll('circle')
+				.filter(function (d) {
+					var x = parseInt(d3.select(this).attr('cx')),
+						y = parseInt(d3.select(this).attr('cy'));
+					return x < -d.r || x > 800 + d.r || (y > 400 && Math.abs(d.y) < 1) || Math.abs(d.x) < 1;
+				})
+				.remove();
+
 			d3.select('svg')
 				.selectAll('circle')
 				.transition()
 				.duration(100)
-				.attr('cx', function(d){
+				.attr('cx', function (d) {
 					return parseInt(d3.select(this).attr('cx')) + d.x;
 				})
-				.attr('cy', function(d){
+				.attr('cy', function (d) {
 					d.y += G;
 					return parseInt(d3.select(this).attr('cy')) + d.y - G / 2;
 				})
-				.filter(function(d, i){
-					var x = parseInt(d3.select(this).attr('cx')),
-						y = parseInt(d3.select(this).attr('cy'));
-					return x < -d.r || x > 800 + d.r || y < -d.r || y > 500 + d.r;
-				})
-				.remove();
+				.each(function (d) {
+					var y = parseInt(d3.select(this).attr('cy'));
+					if (y > 400 && d.y > 0) {
+						d.y = d.y * -0.5;
+						d.x = d.x * 0.9;
+					}
+				});
+
 		},
 
 		createOne = function () {
 			d3.select('svg')
 				.append('circle')
-				.datum({x: -20 + Math.random() * 40, y: -25 * Math.random(), r: 15})
 				.attr({
-					cx: 400, cy: 100, r: 15,
+					cx: 400, cy: 100, r: 10 + Math.random() * 8,
 					fill: function(){
 						return color[Math.floor(Math.random() * 20)];
 					}
+				})
+				.datum({
+					x: -20 + Math.random() * 40, y: -25 * Math.random(), 
+					r: function (d) {return d.r}
 				});
 		},
 
@@ -51,15 +64,15 @@
 				status = 'off';
 				clearInterval(ni);
 				clearInterval(ci);
-				document.getElementById('switch').innerHTML = 'Restart';
+				document.getElementById('switch').innerHTML = 'Continue';
 			}
 		};
 
 	//smarter than forEach
-	Array.apply(null, Array(3)).forEach(function(){ createOne(); });
+	Array.apply(null, Array(1)).forEach(function(){ createOne(); });
 
 	document.getElementById('switch').addEventListener('click', turn);
 
-	var ni = setInterval(nextTick, 50);
-		ci = setInterval(createOne, 200);
+	ni = setInterval(nextTick, 50);
+	ci = setInterval(createOne, 200);
 })()
